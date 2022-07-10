8de3c0f88f19fe9111f06258cf1b4d613999bad7
Ahmet <ahmetrbalkan@gmail.com>
2022-07-10 08:16:30 +0300

    Animation Complete

--- lib/menu_dashboard_widget.dart
+++ lib/menu_dashboard_widget.dart
@@ -3,11 +3,22 @@ import 'package:flutter/material.dart';
 const TextStyle menuStyle = TextStyle(color: Colors.white, fontSize: 20);
 Color backgroundColor = const Color(0xFF333340);
 
-class MenuDashboard extends StatelessWidget {
+class MenuDashboard extends StatefulWidget {
   const MenuDashboard({Key? key}) : super(key: key);
 
   @override
+  State<MenuDashboard> createState() => _MenuDashboardState();
+}
+
+class _MenuDashboardState extends State<MenuDashboard> {
+  late double ekranYuksekligi, ekranGenisligi;
+  late bool menuAcikmi = false;
+
+  @override
   Widget build(BuildContext context) {
+    ekranYuksekligi = MediaQuery.of(context).size.height;
+    ekranGenisligi = MediaQuery.of(context).size.width;
+
     return Scaffold(
       backgroundColor: backgroundColor,
       body: SafeArea(
@@ -67,31 +78,46 @@ class MenuDashboard extends StatelessWidget {
   }
 
   Widget dashboardOlustur(BuildContext context) {
-    return Material(
-      elevation: 8,
-      color: backgroundColor,
-      child: Container(
-        padding: const EdgeInsets.only(left: 16, right: 16, top: 8),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: const [
-                Icon(
-                  Icons.menu,
-                  color: Colors.white,
-                ),
-                Text(
-                  "My Cards",
-                  style: TextStyle(color: Colors.white, fontSize: 24),
-                ),
-                Icon(
-                  Icons.add_circle_outline,
-                  color: Colors.white,
-                ),
-              ],
-            )
-          ],
+    return AnimatedPositioned(
+      top: menuAcikmi ? 0.2 * ekranYuksekligi : 0,
+      bottom: menuAcikmi ? 0.2 * ekranYuksekligi : 0,
+      left: menuAcikmi ? 0.6 * ekranGenisligi : 0,
+      right: menuAcikmi ? -0.4 * ekranGenisligi : 0,
+      duration: const Duration(seconds: 1),
+      child: Material(
+        elevation: 8,
+        color: backgroundColor,
+        child: Container(
+          padding: const EdgeInsets.only(left: 16, right: 16, top: 8),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      setState(() {
+                        menuAcikmi = !menuAcikmi;
+                      });
+                    },
+                    child: const Icon(
+                      Icons.menu,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const Text(
+                    "My Cards",
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                  const Icon(
+                    Icons.add_circle_outline,
+                    color: Colors.white,
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
